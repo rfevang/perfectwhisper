6014bdf65700a5564f0ee1414326e85dfeadccd4
Per Bilet <38363936+Luroleverell@users.noreply.github.com>
2018-04-24 10:24:36 +0200

    Added zones to map and update function

--- action_map.js
+++ action_map.js
@@ -1,6 +1,7 @@
 const SVG_NS = 'http://www.w3.org/2000/svg';
 const MAX_X = 816000;
 const MAX_Y = 816000;
+const ZONEBORDER = 2000;
 
 class ActionMap {
   constructor(slider, match) {
@@ -19,19 +20,29 @@ class ActionMap {
     map.setAttribute('href', 'https://github.com/pubg/api-assets/raw/master/assets/maps/Erangel_Minimap_lowres.jpg');
     this.svg_.appendChild(map);
 
-
     this.playerCircles_ = [];
     match.players().forEach(function(player) {
       let circle = this.createCircleForPlayer_(player);
       this.playerCircles_.push(circle);
       this.svg_.appendChild(circle);
     }, this);
+    
+    this.redZone = document.createElementNS(SVG_NS,"circle");
+    this.redZone.classList.add('redZone');
+    this.svg_.appendChild(this.redZone);
+
+    this.whiteZone = document.createElementNS(SVG_NS,"circle");
+    this.whiteZone.classList.add('whiteZone');
+    this.svg_.appendChild(this.whiteZone);
+
+    this.blueZone = document.createElementNS(SVG_NS,"circle");
+    this.blueZone.classList.add('blueZone');
+    this.svg_.appendChild(this.blueZone);
 
     let viewbox = this.svg_.createSVGRect();
     viewbox.width = MAX_X;
     viewbox.height = MAX_Y;
     this.viewbox = viewbox;
-
 
     slider.addListener(this.onUpdate_.bind(this));
     this.svg_.addEventListener('wheel', this.onMouseWheel_.bind(this));
@@ -47,11 +58,16 @@ class ActionMap {
 
   onUpdate_() {
     let time = this.slider_.getValue();
+    let gameState = this.match_.gameStateAtTime(time);
     this.match_.players().forEach(function(player, index) {
       let pos = player.locationAtTime(time);
       let circle = this.playerCircles_[index];
       let scale = this.viewbox.width / MAX_X;
       circle.setAttribute('transform', 'translate(' + pos.x + ' ' + pos.y + ') scale(' + scale + ')');
+      
+      this.updateZone(this.redZone, gameState.redZone.x, gameState.redZone.y, gameState.redZone.r, gameState.redZone.color, scale);
+      this.updateZone(this.whiteZone, gameState.whiteZone.x, gameState.whiteZone.y, gameState.whiteZone.r, gameState.whiteZone.color, scale);
+      this.updateZone(this.blueZone, gameState.blueZone.x, gameState.blueZone.y, gameState.blueZone.r, gameState.blueZone.color, scale);
     }, this);
   }
 
@@ -135,5 +151,18 @@ class ActionMap {
     ret.width = viewbox.width;
     ret.height = viewbox.height;
     return ret;
+  }
+  
+  updateZone(zone, x, y, r, color, scale){
+    zone.setAttributeNS(null, "cx", this.checkNull(x));
+    zone.setAttributeNS(null, "cy", this.checkNull(y));
+    zone.setAttributeNS(null, "r", this.checkNull(r));
+    zone.setAttributeNS(null, "stroke", color);
+    zone.setAttributeNS(null, "stroke-width", ZONEBORDER * scale);
+  }
+  
+  checkNull(el){
+    if (el == null) return 0
+    return el;
   }
 }
